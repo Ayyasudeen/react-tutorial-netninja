bec354fe968340e8732a49c5323fce4002287f32
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2021-02-12 23:58:33 +0530

    App completed

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry!</h2>
+            <p>The Page Cannot be Found</p>
+            <Link to='/'>Back to the Homepage...</Link>
+        </div>
+     );
+}
+ 
+export default NotFound;
